083ff47672e044d059a3b6d4b7eeae50d12629c5
Edward Norris <etnc6d@mst.edu>
2016-04-13 17:19:27 -0500

    Added a class for the team-level object in the co-ev

---
+++ code/SpacePartition3D.py
@@ -0,0 +1,18 @@
+import time
+
+__author__ = 'etnc6d'
+
+
+class SpacePartition3D(object):
+
+    def __init__(self):
+        self.x = None
+        self.y = None
+        self.z = None
+        fit = -1
+
+    def __repr__(self):
+        return "fit: " + str(self.fit) + "(" + str(len(self.x)) + ',' +  str(len(self.y)) + ',' +str(len(self.z))+ ")\n" + \
+        "x: " + str(self.x) + '\n' + \
+        "y: " + str(self.y) + '\n' + \
+        "z: " + str(self.z)
